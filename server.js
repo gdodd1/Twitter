d89112736b54b90aa3e28601f859c40a787ba1ad
Garrett Dodd <garrettdodd03@gmail.com>
2023-01-05 21:55:40 -0500

    index.ejs

--- server.js
+++ server.js
@@ -4,9 +4,11 @@ const app = express()
 app.get('/', (req, res) => {
   
   res.send('Hello World')
-  
+  let message = 'Hello World'
 })
 
 app.listen(8000 || process.env.port, () =>{
   console.log('https://localhost:8000') 
 })
+
+
