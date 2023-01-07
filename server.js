b4a068b91748f6515950c278d4d93231b09247c8
Garrett Dodd <garrettdodd03@gmail.com>
2023-01-06 20:41:17 -0500

    example ejs files

--- server.js
+++ server.js
@@ -1,14 +1,32 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
-app.get('/', (req, res) => {
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// use res.render to load up an ejs view file
+
+// index page
+app.get('/', function(req, res) {
+
+    const mascots = [
+        { name: 'Sammy', organization: "DigitalOcean", birth_year: 2012},
+        { name: 'Tux', organization: "Linux", birth_year: 1996},
+        { name: 'Moby Dock', organization: "Docker", birth_year: 2013}
+      ];
+    const tagline = "No programming concept is complete without a cute animal mascot.";
+    
+    res.render('pages/index', {
+        mascots: mascots,
+        tagline: tagline
+    });
   
-  res.send('Hello World')
-  let message = 'Hello World'
-})
+});
 
-app.listen(8000 || process.env.port, () =>{
-  console.log('https://localhost:8000') 
-})
+// about page
+app.get('/about', function(req, res) {
+  res.render('pages/about');
+});
 
-
+app.listen(8080);
+console.log('Server is listening on port 8080');
